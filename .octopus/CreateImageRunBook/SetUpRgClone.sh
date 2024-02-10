3f0678972de64d6c2c1570da71c1432cdd10d0cd
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-10 15:30:00 +0000

    OK, that downloads and unzips ok, so cleaning up, but var still not set. ho hum.

--- .octopus/CreateImageRunBook/SetUpRgClone.sh
+++ .octopus/CreateImageRunBook/SetUpRgClone.sh
@@ -2,20 +2,12 @@ RGCLONE_API_ENDPOINT=$(get_octopusvariable "RGCLONE_API_ENDPOINT")
 RGCLONE_ACCESS_TOKEN=$(get_octopusvariable "RGCLONE_ACCESS_TOKEN")
 RGCLONE_DOWNLOAD_URL="$RGCLONE_API_ENDPOINT"
 RGCLONE_DOWNLOAD_URL+="cloning-api/download/cli/linux-amd64"
-
 echo "RGCLONE_API_ENDPOINT is: $RGCLONE_API_ENDPOINT"
 echo "RGCLONE_DOWNLOAD_URL is: $RGCLONE_DOWNLOAD_URL"
-
 echo "Downloading and unzipping rgclone from $RGCLONE_DOWNLOAD_URL"
-
 curl $RGCLONE_DOWNLOAD_URL | tar xz
-
-echo "Running ls"
-
+echo "Running ls to see if rgclone is there"
 ls
-
 echo "Attempting to use rgclone"
-
 ./rgclone version
-
 ./rgclone get all
