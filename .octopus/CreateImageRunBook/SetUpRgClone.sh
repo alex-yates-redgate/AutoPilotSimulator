bc2c7e4f384a284ca2a32c161ecb62674995bc0b
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-10 15:33:29 +0000

    Doing an extra EXPORT RGCLONE_API_ENDPOINT

--- .octopus/CreateImageRunBook/SetUpRgClone.sh
+++ .octopus/CreateImageRunBook/SetUpRgClone.sh
@@ -8,6 +8,8 @@ echo "Downloading and unzipping rgclone from $RGCLONE_DOWNLOAD_URL"
 curl $RGCLONE_DOWNLOAD_URL | tar xz
 echo "Running ls to see if rgclone is there"
 ls
+echo "Exporting RGCLONE_API_ENDPOINT"
+export RGCLONE_API_ENDPOINT=RGCLONE_API_ENDPOINT
 echo "Attempting to use rgclone"
 ./rgclone version
 ./rgclone get all
