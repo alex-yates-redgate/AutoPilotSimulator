d8fd06d7497f3eec6c56632bed95bd34c71c9be1
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-10 15:10:29 +0000

    More logging

--- .octopus/CreateImageRunBook/SetUpRgClone.sh
+++ .octopus/CreateImageRunBook/SetUpRgClone.sh
@@ -3,9 +3,22 @@ RGCLONE_ACCESS_TOKEN=$(get_octopusvariable "RGCLONE_ACCESS_TOKEN")
 
 echo "RGCLONE_API_ENDPOINT is: $RGCLONE_API_ENDPOINT"
 
-curl $RGCLONE_API_ENDPOINT/cloning-api/download/cli/linux-amd64 | tar xz
+echo "Downloading rgclone"
+
+curl $RGCLONE_API_ENDPOINT/cloning-api/download/cli/linux-amd64 
+
+echo "Running ls"
 
 ls
+
+echo "Unzipping rgclone"
+tar xz rgclone.tar.gz
+
+echo "Running ls"
+
+ls
+
+echo "Attempting to use rgclone"
 
 ./rgclone version
 
