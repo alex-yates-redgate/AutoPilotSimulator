54664b5f18366a839acea5e8396e5d2bd843f397
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-10 15:19:01 +0000

    I think the problem was a double /. Hopefully this fixes it.

--- .octopus/CreateImageRunBook/SetUpRgClone.sh
+++ .octopus/CreateImageRunBook/SetUpRgClone.sh
@@ -1,11 +1,13 @@
 RGCLONE_API_ENDPOINT=$(get_octopusvariable "RGCLONE_API_ENDPOINT")
 RGCLONE_ACCESS_TOKEN=$(get_octopusvariable "RGCLONE_ACCESS_TOKEN")
+RGCLONE_DOWNLOAD_URL=$RGCLONE_API_ENDPOINT + "cloning-api/download/cli/linux-amd64"
 
 echo "RGCLONE_API_ENDPOINT is: $RGCLONE_API_ENDPOINT"
+echo "RGCLONE_DOWNLOAD_URL is: $RGCLONE_DOWNLOAD_URL"
 
-echo "Downloading rgclone from $RGCLONE_API_ENDPOINT/cloning-api/download/cli/linux-amd64"
+echo "Downloading rgclone from $RGCLONE_DOWNLOAD_URL"
 
-curl $RGCLONE_API_ENDPOINT/cloning-api/download/cli/linux-amd64 
+curl $RGCLONE_DOWNLOAD_URL 
 
 echo "Running ls"
 
