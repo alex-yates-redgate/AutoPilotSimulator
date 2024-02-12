35caaee8bee0b2143d6111628d2b9bc31b788f2d
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-12 12:47:10 +0000

    naming things is hard

--- .octopus/CreateImageRunBook/CreateDbEnvironment_2_UpdateProjVars.ps1
+++ .octopus/CreateImageRunBook/CreateDbEnvironment_2_UpdateProjVars.ps1
@@ -3,5 +3,5 @@ octopus config set url $OctopusParameters["Octopus.Web.ServerUri"]
 octopus config set apikey $OctopusParameters["OCTOPUS_API_KEY"]
 
 # Create the database environment, and save the JDBC connection string
-$jdbc = $OctopusParameters["Octopus.Action[Create environment].Output.jdbcConnnectionString"]
+$jdbc = $OctopusParameters["Octopus.Action[Create rgclone data container].Output.jdbcConnnectionString"]
 octopus project variables update --project $OctopusParameters["Octopus.Project.Name"] --name "DB_CONNECTION_STRING" --value "$jdbc" --environment-scope $OctopusParameters["Octopus.Environment.Name"] --no-prompt
