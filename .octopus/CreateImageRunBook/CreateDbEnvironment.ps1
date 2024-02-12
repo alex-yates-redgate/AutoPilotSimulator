7dbb6bee7667989c12f89fd00b7a70b3952f4d66
Alex Yates (Redgate) <140410709+alex-yates-redgate@users.noreply.github.com>
2024-02-12 12:11:16 +0000

    Managing env variables in ps

--- .octopus/CreateImageRunBook/CreateDbEnvironment.ps1
+++ .octopus/CreateImageRunBook/CreateDbEnvironment.ps1
@@ -1,5 +1,6 @@
+[Environment]::SetEnvironmentVariable('RGCLONE_ACCESS_TOKEN', $RGCLONE_ACCESS_TOKEN)
+[Environment]::SetEnvironmentVariable('RGCLONE_API_ENDPOINT', $RGCLONE_API_ENDPOINT)
+
 $dcInfo = (~/redgate/rgclone create dc -i Widget -o json) | ConvertFrom-Json
-
 $jdbc = $dcInfo.jdbcConnectionString
-
 Write-Output "JDBC = $jdbc"
