1d39b1f1ee5b2e29788997a82b07c645c160be89
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-14 14:50:11 +0000

    Adding a separate script for data image refrash

---
+++ .octopus/CreateImageRunBook/RefreshWidgetDataImage.sh
@@ -0,0 +1,14 @@
+export RGCLONE_API_ENDPOINT=$(get_octopusvariable "RGCLONE_API_ENDPOINT")
+export RGCLONE_ACCESS_TOKEN=$(get_octopusvariable "RGCLONE_ACCESS_TOKEN")
+
+echo "Creating new Widget data image:"
+~/redgate/rgclone create di -f ./Widget.yaml --name Widget_New
+
+echo "Deleting previous Widget data image:"
+#~/redgate/rgclone delete di -f ./Widget_Previous.yaml
+
+echo "Renaming current Widget data image to Widget_Previous:"
+#~/redgate/rgclone update di Widget --name Widget_Previous
+
+echo "Renaming new Widget data image to Widget:"
+~/redgate/rgclone update di Widget_New --name Widget
