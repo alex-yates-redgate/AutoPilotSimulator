3d03fb675d78a9588ff5308eb744f78c02d707c4
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-16 15:56:39 +0000

    Seems to download ok, but the copy part of script seems to require a pwd. Attmpting to work around.

--- .octopus/CreateImageRunBook/SetUpRgClone.sh
+++ .octopus/CreateImageRunBook/SetUpRgClone.sh
@@ -17,7 +17,10 @@ echo "rgclone version is:"
 ~/redgate/rgclone version
 
 echo "Installing flyway"
-wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/8.5.13/flyway-commandline-8.5.13-linux-x64.tar.gz | tar xvz && sudo ln -s `pwd`/flyway-8.5.13/flyway /usr/local/bin 
+wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/8.5.13/flyway-commandline-8.5.13-linux-x64.tar.gz | tar xvz #&& sudo ln -s `pwd`/flyway-8.5.13/flyway /usr/local/bin 
+
+dir
+
 echo "Copying flyway to persistent dir"
 mv `pwd`/flyway-8.5.13/flyway ~/redgate/flyway -f # copies rgclone to persistent dir, and overwrites existing file
 
