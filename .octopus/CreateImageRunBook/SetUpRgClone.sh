10952c8f7a31d2f6ceaf43f58eca05b49648b900
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-16 15:52:40 +0000

    Attempting to also install Flyway

--- .octopus/CreateImageRunBook/SetUpRgClone.sh
+++ .octopus/CreateImageRunBook/SetUpRgClone.sh
@@ -10,8 +10,16 @@ curl $RGCLONE_DOWNLOAD_URL | tar xz
 # undoing the stdout hack above
 echo "##octopus[stderr-default]"
 
-mkdir ~/redgate -p # cretes aredgate dir (if not exists)
+mkdir ~/redgate -p # cretes a redgate dir (if not exists)
 mv ./rgclone ~/redgate/rgclone -f # copies rgclone to persistent dir, and overwrites existing file
 
 echo "rgclone version is:"
 ~/redgate/rgclone version
+
+echo "Installing flyway"
+wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/8.5.13/flyway-commandline-8.5.13-linux-x64.tar.gz | tar xvz && sudo ln -s `pwd`/flyway-8.5.13/flyway /usr/local/bin 
+echo "Copying flyway to persistent dir"
+mv `pwd`/flyway-8.5.13/flyway ~/redgate/flyway -f # copies rgclone to persistent dir, and overwrites existing file
+
+echo "Trying flyway version"
+./redgate/flyway version
