c1e7cf9684e5e3bd0a2fed5aa70a9d71f4e9983c
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-19 15:34:24 +0000

    Data images now start with a FSH table, and connection strings are outputted correctly

--- .octopus/CreateImageRunBook/CreateDbEnvironment_1_RgcloneCreateDc.ps1
+++ .octopus/CreateImageRunBook/CreateDbEnvironment_1_RgcloneCreateDc.ps1
@@ -2,8 +2,16 @@
 [Environment]::SetEnvironmentVariable('RGCLONE_ACCESS_TOKEN', $RGCLONE_ACCESS_TOKEN)
 [Environment]::SetEnvironmentVariable('RGCLONE_API_ENDPOINT', $RGCLONE_API_ENDPOINT)
 
-# Create the database environment, and save the JDBC connection string
-$dcInfo = (~/redgate/rgclone create dc -i Widget -o json) | ConvertFrom-Json
-$jdbc = $dcInfo.jdbcConnectionString
+$containerName = "Widget-" + $OctopusParameters["Octopus.Environment.Name"]
+# redirect output to stdout to avoid errors/warnings
+echo "##octopus[stderr-progress]"
+~/redgate/rgclone update dc $containerName -n "$containerName-previous" -t 30m
+# undoing the stdout hack above
+echo "##octopus[stderr-default]"
 
-Set-OctopusVariable -Name jdbcConnectionString -Value "$jdbc" -Sensitive
+# Create the database environment, and save the JDBC connection string
+$dcInfo = (~/redgate/rgclone create dc -i Widget --name "$containerName" -o json) | ConvertFrom-Json
+$jdbc = $dcInfo.jdbcConnectionString
+$jdbcWithDbName = $jdbc + "databaseName=WidgetProduction"
+
+Set-OctopusVariable -Name jdbcConnectionString -Value "$jdbcWithDbName" -Sensitive
