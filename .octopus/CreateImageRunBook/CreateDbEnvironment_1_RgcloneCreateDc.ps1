2d4febb3fed14f8699799002be6ddec8a2bd2123
alex-yates-redgate <alex.yates@red-gate.com>
2024-02-19 16:01:11 +0000

    Case sensitivity appears to be important. Also environments live for 3 hours.

--- .octopus/CreateImageRunBook/CreateDbEnvironment_1_RgcloneCreateDc.ps1
+++ .octopus/CreateImageRunBook/CreateDbEnvironment_1_RgcloneCreateDc.ps1
@@ -10,8 +10,8 @@ echo "##octopus[stderr-progress]"
 echo "##octopus[stderr-default]"
 
 # Create the database environment, and save the JDBC connection string
-$dcInfo = (~/redgate/rgclone create dc -i Widget --name "$containerName" -t 1h -o json) | ConvertFrom-Json
+$dcInfo = (~/redgate/rgclone create dc -i Widget --name "$containerName" -t 3h -o json) | ConvertFrom-Json
 $jdbc = $dcInfo.jdbcConnectionString
-$jdbcWithDbName = $jdbc + "databaseName=WidgetProduction"
+$jdbcWithDbName = $jdbc + "DatabaseName=WidgetProduction"
 
 Set-OctopusVariable -Name jdbcConnectionString -Value "$jdbcWithDbName" -Sensitive
